018da9036db19d1755f875204efa1bbbe4714992
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2020-10-21 20:31:07 -0500

    made some vars to test the tsc

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 "use strict";
-console.log("hello world");
+var myName = "Stu";
+var age = 36;
+console.log("Hi! I'm " + myName + ", and I'm " + age + " years old.");
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,6 @@
-console.log("hello world");
+
+let myName : string = "Stu";
+let age :  number = 36;
+
+console.log(`Hi! I'm ${myName}, and I'm ${age} years old.`);
+
