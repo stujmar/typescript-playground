ac35942008107aa7ae45df4b9a7751e553028cf8
Stuart Marsh <stujmar@gmail.com>
2023-01-19 18:59:40 -0600

    [sm] more with dynamic and flexible type definitions

--- li-le-typescript/src/dynamic.ts
+++ li-le-typescript/src/dynamic.ts
@@ -1,2 +1,7 @@
-let batman  = {name: "Wruce" , age: 42};
+let batman: any  = {name: "Wruce" , age: 42}; // Opting out of type checking
 batman.id = 1234;
+
+let superman: Record<string, string | number> = {name: "Clark" , age: 42}; // flexible types.
+superman.id = 1234;
+
+superman = {name: true , age: 42, id: 1234, address: "123 Main St." }; // error on flexible types.
